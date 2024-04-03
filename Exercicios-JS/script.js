2bc7299e80ceaaa7447e5f0f0427ac01dd74a78e
LeonardoLobas <leonardolobas2703@gmail.com>
2024-04-03 14:40:06 -0300

    Tipos de dados exercicio.

--- Exercicios-JS/script.js
+++ Exercicios-JS/script.js
@@ -1,11 +1,24 @@
-const nome = "Leonardo"
-const idade = "21"
+// Declare uma variável contendo uma string
+var substantivo = "Leonel";
 
-console.log(nome,idade)
+// Declare uma variável contendo um número dentro de uma string
+var numero = "21";
 
-const comida = "Lasanha"
+// Declare uma variável com a sua idade
+var idade = 21;
 
-console.log(comida)
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = "Leonardo";
 
-const carro =
+var  sobrenome = "Lobas";
 
+var nomeCompleto = `${nome} ${sobrenome}`;
+
+console.log(nomeCompleto);
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = `It's time`;
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);

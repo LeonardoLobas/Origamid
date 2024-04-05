3a33493b5cc08546aebc65f3d6b94170cf997422
LeonardoLobas <leonardolobas2703@gmail.com>
2024-04-05 16:25:40 -0300

    Boolean e condicionais exercicios.

---
+++ Boolean e Condicionais/script.js
@@ -0,0 +1,18 @@
+var pergunta = prompt("Você possui graduação?");
+
+if (pergunta === "sim" ) {
+    console.log("Você é graduado");
+
+}else if (pergunta === "não"){
+    console.log("Você não é graduado");
+
+}else {
+    console.log("Responda com 'sim' ou 'não' ");
+}
+  
+
+
+
+
+
+

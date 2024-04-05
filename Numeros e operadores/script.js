9bfc31d402b4b719ea739ee3b95f65f48c753981
LeonardoLobas <leonardolobas2703@gmail.com>
2024-04-05 14:17:47 -0300

    Numeros e operadores exercicios

---
+++ Numeros e operadores/script.js
@@ -0,0 +1,29 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20; 35
+
+// Crie duas expressões que retornem NaN
+
+var substantivo = "Leonardo";
+var verbo = "Jogar";
+console.log(isNaN(substantivo));
+
+// Somar a string '200' com o número 50 e retornar 250
+
+var somar = +"200" + 50;
+console.log(somar);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+
+var incremento = 27;
+console.log(++incremento)
+
+// Como dividir o peso por 2?
+
+var peso = 78
+var unidade = "kg"
+
+var divisao = peso / 2 + unidade
+console.log(divisao)
+
+
+

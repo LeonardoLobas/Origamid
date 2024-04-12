709a25b650a0ac824de07fb04ef0618d37a4eca6
LeonardoLobas <leonardolobas2703@gmail.com>
2024-04-12 11:50:35 -0300

    Atribuição e Ternário.

---
+++ Atribuição e Ternário/script.js
@@ -0,0 +1,28 @@
+// Some 500 ao valor de scroll abaixo,
+// atribuindo o novo valor a scroll
+
+var scroll = 1000;
+scroll += 500
+console.log(scroll)
+
+// Atribua true para a variável darCredito,
+// caso o cliente possua carro e casa.
+// E false caso o contrário.
+
+var possuiCarro = true;
+var possuiCasa = true;
+
+
+// var darCredito = (possuiCarro && possuiCasa) ? "Você possui credito" : "Você não possui credito!";
+
+// console.log(darCredito);
+
+var darCredito = true 
+if (darCredito ===  possuiCasa && possuiCarro) {
+    console.log('Você possui credito!')
+}
+
+else {
+    console.log("Você não possui credito!")
+}
+

0fea1042f3ef8281235f7ccbf72582814a35fd5b
LeonardoLobas <leonardolobas2703@gmail.com>
2024-04-12 15:04:06 -0300

    Manipulando elementos DOM

---
+++ O que é o DOM/script.js
@@ -0,0 +1,20 @@
+// Retorne o url da página atual utilizando o objeto window
+
+const hrefPagina = window.location.href
+    console.log(hrefPagina);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const link = document.querySelector(".ativo");
+    console.log(link)
+   
+
+
+// Retorne a linguagem do navegador
+const linguagem = window.navigator.language;
+    console.log(linguagem);
+
+
+// Retorne a largura da janela 
+const largura = window.innerWidth;
+    console.log(largura)

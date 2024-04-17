fb9830651c541fa3923176f274bd60a1df8774b8
LeonardoLobas <leonardolobas2703@gmail.com>
2024-04-17 09:34:48 -0300

    ForEach e Arrow Function. EXERCIOCIO 2

--- Animais Fantasticos/script.js
+++ Animais Fantasticos/script.js
@@ -1,25 +1,28 @@
-// Retorne no console todas as imagens do site
-const selectimagens = document.querySelectorAll("img")
- 
-console.log(selectimagens)
+// Mostre no console cada parágrado do site
 
-// Retorne no console apenas as imagens que começaram com a palavra imagem
+const paragrafo = document.querySelectorAll("p")
+const arrayparagrafo = Array.from(paragrafo)
 
-const imagensAnimais = document.querySelectorAll("img[src^='/Animais Fantasticos/img/imagem']")
-console.log(imagensAnimais)
+arrayparagrafo.forEach(() => {console.log(arrayparagrafo)})
 
-// Selecione todos os links internos (onde o href começa com #)
+// Mostre o texto dos parágrafos no console
 
-const linksinternos = document.querySelectorAll("[href^='#']")
-console.log(linksinternos)
-
-// Selecione o primeiro h2 dentro de .animais-descricao
-
-const primerioh2 = document.querySelector(".animais-descricao h2")
-console.log(primerioh2)
-
-// Selecione o último p do site
+arrayparagrafo.forEach((item) => {console.log(item.innerText)})
 
 
-const ultimop = document.querySelectorAll("p");
-console.log(ultimop[ultimop.length]);
+// Como corrigir os erros abaixo:
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item, index) => {
+  console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach(()=> {
+    console.log(i++);
+  }); 
+
+imgs.forEach(() => i++);
+
+
+

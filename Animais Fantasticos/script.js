8981d391ac8e870e809e2564534641ddb9aa0961
LeonardoLobas <leonardolobas2703@gmail.com>
2024-04-18 17:56:17 -0300

    Dimensões e distancias. EXE 3

--- Animais Fantasticos/script.js
+++ Animais Fantasticos/script.js
@@ -1,32 +1,49 @@
-// Adicione a classe ativo a todos os itens do menu
+// Verifique a distância da primeira imagem
+// em relação ao topo da página
 
-const itensmenu = document.querySelectorAll(".menu a")
-itensmenu.forEach((item) => {
-  item.classList.add('ativo');
+const DistFirstImg = document.querySelector(".animais-lista")
+
+console.log(DistFirstImg.offsetTop)
+
+// Retorne a soma da largura de todas as imagens
+
+  const imagens = document.querySelectorAll('img');
+  let soma = 0 
+  imagens.forEach((imagem) => {
+    soma += imagem.offsetWidth
+  })
+  console.log(soma)
+
+
+
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
+
+const links = document.querySelectorAll('a')
+
+links.forEach((link) => {
+  const width = link.offsetWidth;
+  const height = link.offsetHeight;
+  if(width >= 48 && height >= 48) {
+    console.log(link, 'Possui boa acessibilidade')
+  } else {
+    console.log(link, 'Não possui boa acessibilidade')
+  }
+  console.log(height)
 })
+// Se o browser for menor que 720px,
+// adicione a classe menu-mobile ao menu
 
-// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+const small = window.matchMedia('(max-width: 720px)').matches
+const menu = document.querySelector('.menu')
 
-itensmenu.forEach((item) => {
-  item.classList.remove('ativo');
-})
+if (small) { 
+  menu.classList.add('menu-mobile')
+} else {
+  menu.classList.remove('menu-mobile')
+}
 
-itensmenu[0].classList.add("ativo")
-
-// Verifique se as imagens possuem o atributo alt
-const imgs = document.querySelectorAll("img");
-
-imgs.forEach((img) => {
-  const possuialt = img.hasAttribute("alt")
-  console.log(possuialt)
-})
-
-// Modifique o href do link externo no menu
-
-const link = document.querySelector("a[href^='https']")
-link.setAttribute("href", )
-
-
-
+console.log(small)
 
 

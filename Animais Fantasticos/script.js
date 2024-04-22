ee663bfed93133908aa1e28254925831f4e8a081
LeonardoLobas <leonardolobas2703@gmail.com>
2024-04-22 17:56:18 -0300

    Animações listas. Animais/FAQ.

---
+++ Animais Fantasticos/script.js
@@ -0,0 +1,46 @@
+function initTabNav(){
+    const tabmenu = document.querySelectorAll('.js-tabmenu li');
+    const tabcontent = document.querySelectorAll('.js-tabcontent section');
+    tabcontent[0].classList.add('ativo')
+
+
+    if (tabmenu.length && tabcontent.length) {
+        function activeTab(index) {
+            tabcontent.forEach((section) => {
+                section.classList.remove('ativo');
+            })
+            tabcontent[index].classList.add('ativo');
+
+        }
+
+        tabmenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', function () {
+                activeTab(index)
+            })
+        });
+    }
+}
+initTabNav();
+
+
+// ---------------FAQ---------------------
+
+function initAccordion(){
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const activeClass = 'ativo';
+    if (accordionList.length) {
+        accordionList[0].classList.add(activeClass)
+        accordionList[0].nextElementSibling.classList.add(activeClass)
+
+        function activeAccordion() {
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass);
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        });
+    }
+}
+
+initAccordion();

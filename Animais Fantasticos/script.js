f7f37088b9b382859ce21bcd6e3339b85ab0477d
LeonardoLobas <leonardolobas2703@gmail.com>
2024-04-23 09:38:18 -0300

    Links internos Scroll.

--- Animais Fantasticos/script.js
+++ Animais Fantasticos/script.js
@@ -44,3 +44,31 @@ function initAccordion(){
 }
 
 initAccordion();
+
+//---------------------------LINKS INTERNOS SCROLL--------------------------------
+function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"')
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href)
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection)
+    })
+}
+
+initScrollSuave();
+
+const sections = document.querySelectorAll('js-scroll')
+
+function animaScroll(){
+    console.log('sim');
+}
+
+window.addEventListener('scroll', animaScroll)

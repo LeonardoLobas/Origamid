7de02ad47a49e74f17d6d29089bd810140ee8184
LeonardoLobas <leonardolobas2703@gmail.com>
2024-04-24 10:18:38 -0300

    Contructor Function.

---
+++ Exercicios/Constructor Function/ExerciciosAnimaisfantasticos.js
@@ -0,0 +1,32 @@
+// Transforme o objeto abaixo em uma Constructor Function
+function Pessoa(nome, idade) {
+  this.nome = nome;
+  this.idade = idade;
+  this.andar = function () {
+    console.log(nome + ' andou')
+  }
+} 
+
+//   // Crie 3 pessoas, João - 20 anos,
+//   // Maria - 25 anos, Bruno - 15 anos
+
+  const João = new Pessoa('João', 20)
+  const Maria = new Pessoa('Maria', 25)
+  const Bruno = new Pessoa('Bruno', 15)
+  
+  
+  // Crie uma Constructor Function (Dom) para manipulação
+  // de listas de elementos do dom. Deve conter as seguintes
+  // propriedades e métodos:
+  //
+  // elements, retorna NodeList com os elementos selecionados
+  // addClass(classe), adiciona a classe a todos os elementos
+  // removeClass(classe), remove a classe a todos os elementos
+  
+  function Dom(seletor) {
+    const elementList = document.querySelectorAll(seletor)
+    console.log(seletor)
+  }
+  
+
+  

9a1337eee418e798fbaa0c7af7f8c727348c429e
LeonardoLobas <leonardolobas2703@gmail.com>
2024-04-24 11:19:22 -0300

    Contructor Function.

--- Exercicios/Constructor Function/ExerciciosAnimaisfantasticos.js
+++ Exercicios/Constructor Function/ExerciciosAnimaisfantasticos.js
@@ -23,10 +23,21 @@ function Pessoa(nome, idade) {
   // addClass(classe), adiciona a classe a todos os elementos
   // removeClass(classe), remove a classe a todos os elementos
   
-  function Dom(seletor) {
-    const elementList = document.querySelectorAll(seletor)
-    console.log(seletor)
+function Dom(seletor){
+  const elementList = document.querySelectorAll(seletor);
+  this.elements = elementList;
+  this.addClass = function(classe){
+    elementList.forEach((element) => {
+      element.classList.add(classe);
+    })
+  }
+  this.removeClass = function(classe){
+    elementList.forEach((element) => {
+      element.classList.remove(classe);
+    })
   }
   
+}
 
+const listaItens = new Dom('li')
   

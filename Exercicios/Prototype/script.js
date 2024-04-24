267116a6514ffe5f65eb1d04dd8ad02c2fe1daa6
LeonardoLobas <leonardolobas2703@gmail.com>
2024-04-24 16:34:16 -0300

    Prototype exercicios.

---
+++ Exercicios/Prototype/script.js
@@ -0,0 +1,34 @@
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+
+function Pessoas(nome,sobrenome,idade, ) {
+    this.nome = nome,
+    this.sobrenome = sobrenome,
+    this.idade = idade
+    
+}
+
+Pessoas.prototype.nomeCompleto = function() {
+    return `${this.nome} ${this.sobrenome}`
+}
+const leonardo = new Pessoas('Leonardo','Lobas',21)
+console.log(leonardo.nomeCompleto())
+
+// Liste os métodos acessados por 
+// dados criados com NodeList,
+// HTMLCollection, Document
+// Liste os construtores dos dados abaixo
+const li = document.querySelector('li');
+
+// li;
+// li.click;
+// li.innerText;
+// li.value;
+// li.hidden;
+// li.offsetLeft;
+// li.click();
+
+// Qual o construtor do dado abaixo:
+// li.hidden.constructor.name;

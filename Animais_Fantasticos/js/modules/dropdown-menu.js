a6d8cba01bb8b83a4e2a085726db4b84bbf00e49
LeonardoLobas <leonardolobas2703@gmail.com>
2024-05-14 11:10:02 -0300

    Click Outside.

--- Animais_Fantasticos/js/modules/dropdown-menu.js
+++ Animais_Fantasticos/js/modules/dropdown-menu.js
@@ -9,7 +9,18 @@ export default function initDropDown() {
     
     function handleClick(event) {
         event.preventDefault();
-        this.classList.toggle('ativo')
-    
+        this.classList.add('ativo');
+        outsideClick(this, () => {
+            this.classList.remove('ativo')
+        })
+    }
+    function outsideClick(element,callback){
+        const html = document.documentElement;
+        html.addEventListener('click', handleOutsideClick);
+        function handleOutsideClick(event) {
+            if(!element.contains(event.target)){
+                callback();
+            }
+        }
     }
 }

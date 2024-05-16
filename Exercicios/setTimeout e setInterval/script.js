ed8ce3d271779e75358c19aa59f99869fc55270c
LeonardoLobas <leonardolobas2703@gmail.com>
2024-05-16 16:08:35 -0300

    setTimeout e setInterval.

--- Exercicios/setTimeout e setInterval/script.js
+++ Exercicios/setTimeout e setInterval/script.js
@@ -1,19 +1,42 @@
 // Mude a cor da tela para azul e depois para vermelho a cada 2s.
 
-const body = document.querySelector('body');
-let isBlue = false;
+// function mudaClasse() {
+//     document.body.classList.toggle('ativoRed')
+// }
 
-setInterval(function() {
-    if (isBlue) {
-        body.style.backgroundColor = 'red';
-    } else {
-        body.style.backgroundColor = 'blue';
-    }
-    isBlue = !isBlue;
-}, 2000);
-
+// setInterval(mudaClasse,2000)
 
 
 
 // Crie um cronometro utilizando o setInterval. Deve ser possível
 // iniciar, pausar e resetar (duplo clique no pausar).
+
+const iniciar = document.querySelector('.iniciar')
+const pausar = document.querySelector('.pausar')
+const tempo = document.querySelector('.tempo')
+
+iniciar.addEventListener('click',iniciarTempo)
+pausar.addEventListener('click',pausarTempo)
+pausar.addEventListener('dblclick',resetarTempo)
+
+
+let i = 0;
+let timer
+
+
+function iniciarTempo(){
+    timer = setInterval(() => {
+        tempo.innerText = i++;
+    }, 100);
+    iniciar.setAttribute('disabled', '');
+}
+
+function pausarTempo() {
+    clearInterval(timer);
+     iniciar.removeAttribute('disabled')
+}
+
+function resetarTempo(){
+    tempo.innerText = 0;
+    i = 0;
+}

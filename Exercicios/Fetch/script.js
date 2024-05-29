52602f1d372a901fdd77474b0559bb1363622409
LeonardoLobas <leonardolobas2703@gmail.com>
2024-05-29 17:16:46 -0300

    BitCoin API.

--- Exercicios/Fetch/script.js
+++ Exercicios/Fetch/script.js
@@ -1,25 +1,18 @@
-// Utilizando a API https://viacep.com.br/ws/${CEP}/json/
-// crie um formulário onde o usuário pode digitar o cep
-// e o endereço completo é retornado ao clicar em buscar
 
+// Utilizando a API https://blockchain.info/ticker
+// retorne no DOM o valor de compra da bitcoin and reais.
+// atualize este valor a cada 30s
 
-const inputCep = document.getElementById('cep');
-const btnCep = document.getElementById('btnCep');
-const resultadoCep = document.querySelector('.resultadoCep')
+const btcDisplay = document.querySelector('.btc');
 
-btnCep.addEventListener('click', handleClick);
-
-function handleClick(event) {
-    event.preventDefault();
-    const cep = inputCep.value;
-    buscaCep(cep)
-    console.log(event)
-}
-
-function buscaCep(cep) {
-    fetch(`https://viacep.com.br/ws/${cep}/json`)
-    .then(response => response.text())
-    .then(body => {
-        resultadoCep.innerText = body;
+function fetchBtc() {
+    fetch('https://blockchain.info/ticker')
+    .then(response => response.json())
+    .then(btcJson => {
+        btcDisplay.innerText = ('R$' + btcJson.BRL.buy).replace('.',',');
     })
 }
+
+setInterval(fetchBtc,3000)
+
+fetchBtc();

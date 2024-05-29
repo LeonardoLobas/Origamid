1f7283a631eb6e609cf821222b5869e0713e0a6f
LeonardoLobas <leonardolobas2703@gmail.com>
2024-05-29 16:16:16 -0300

    Api Via cep.

--- Exercicios/Fetch/script.js
+++ Exercicios/Fetch/script.js
@@ -1,7 +1,25 @@
-const cep = fetch('https://viacep.com.br/ws/01001000/json/');
+// Utilizando a API https://viacep.com.br/ws/${CEP}/json/
+// crie um formulário onde o usuário pode digitar o cep
+// e o endereço completo é retornado ao clicar em buscar
 
-cep.then(r => r.json())
-.then(body => {
-    console.log(body);
-    const conteudo = document.querySelector('.conteudo')
-})
+
+const inputCep = document.getElementById('cep');
+const btnCep = document.getElementById('btnCep');
+const resultadoCep = document.querySelector('.resultadoCep')
+
+btnCep.addEventListener('click', handleClick);
+
+function handleClick(event) {
+    event.preventDefault();
+    const cep = inputCep.value;
+    buscaCep(cep)
+    console.log(event)
+}
+
+function buscaCep(cep) {
+    fetch(`https://viacep.com.br/ws/${cep}/json`)
+    .then(response => response.text())
+    .then(body => {
+        resultadoCep.innerText = body;
+    })
+}

d0294745fc310253f266f3f7c9c2bca875808ffc
LeonardoLobas <leonardolobas2703@gmail.com>
2024-06-13 10:24:48 -0300

    TabNav, refatorado.

--- Animais_Fantasticos/js/modules/tabnav.js
+++ Animais_Fantasticos/js/modules/tabnav.js
@@ -1,23 +1,31 @@
-export default function initTabNav(){
-    const tabmenu = document.querySelectorAll('[data-tab="menu"] li');
-    const tabcontent = document.querySelectorAll('[data-tab="content"] section');
-    tabcontent[0].classList.add('ativo')
+export default class TabNav {
+    constructor(menu, content) {
+        this.tabmenu = document.querySelectorAll(menu);
+        this.tabcontent = document.querySelectorAll(content);
+        this.activeClass = 'ativo'
+    }
 
 
-    if (tabmenu.length && tabcontent.length) {
-        function activeTab(index) {
-            tabcontent.forEach((section) => {
-                section.classList.remove('ativo');
-            })
-            const direcao = tabcontent[index].dataset.anime;
-            tabcontent[index].classList.add('ativo', direcao);
+    activeTab(index) {
+        this.tabcontent.forEach((section) => {
+            section.classList.remove(this.activeClass);
+        })
+        const direcao = this.tabcontent[index].dataset.anime;
+        this.tabcontent[index].classList.add(this.activeClass, direcao);
 
-        }
+    }
 
-        tabmenu.forEach((itemMenu, index) => {
-            itemMenu.addEventListener('click', function () {
-                activeTab(index)
-            })
+    addTabNavEvent(){
+        this.tabmenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => this.activeTab(index))
         });
     }
+
+    init(){
+        if(this.tabmenu.length && this.tabcontent.length) {
+            this.addTabNavEvent();
+            this.activeTab(0)
+        }
+    }
+
 }

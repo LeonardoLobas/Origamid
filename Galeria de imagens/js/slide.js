1d786293daa53a73a31c0d73a255c3bdb31f9309
LeonardoLobas <leonardolobas2703@gmail.com>
2024-06-19 11:53:19 -0300

    Galeria de imagens.

--- Galeria de imagens/js/slide.js
+++ Galeria de imagens/js/slide.js
@@ -2,5 +2,42 @@ export default class Slide {
     constructor(slide, wrapper) {
         this.slide = document.querySelector(slide)
         this.wrapper = document.querySelector(wrapper)
+        this.dist = { finalPosition: 0, startX: 0, movement: 0 }
+    }
+    moveSlide(distX) {
+        this.slide.style.transform = `translate3d(${distX}px, 0, 0)`
+        this.dist.movePosition = distX
+    }
+    upDatePosition(clientX) {
+        this.dist.movement = (this.dist.startX - clientX) * 1.6
+        return this.dist.finalPosition - this.dist.movement
+    }
+    onStart(event) {
+        event.preventDefault()
+        this.wrapper.addEventListener('mousemove', this.onMove)
+        this.dist.startX = event.clientX;
+    }
+    addSlideEvent() {
+        this.wrapper.addEventListener('mousedown', this.onStart)
+        this.wrapper.addEventListener('mouseup', this.onEnd)
+    }
+    onMove(event) {
+        const finalPosition = this.upDatePosition(event.clientX)
+        this.moveSlide(finalPosition)
+    }
+    onEnd(event) {
+        this.wrapper.removeEventListener('mousemove', this.onMove)
+        this.dist.finalPosition = this.dist.movePosition
+    }
+    bindEvents() {
+        this.onStart = this.onStart.bind(this)
+        this.onMove = this.onMove.bind(this)
+        this.onEnd = this.onEnd.bind(this)
+    }
+
+    init() {
+        this.bindEvents();
+        this.addSlideEvent();
+        return this;
     }
 }
